f7ebf0d483a9e69915a3fb8a67d2364711f197b5
shangfengli <shangfengli@tencent.com>
2022-12-16 13:12:53 +0800

    add dockerfile

---
+++ 01/docker-image.md
@@ -0,0 +1,7 @@
+
+#### Docker 镜像
+
+
+--- 
+参考文档：
+1) [Dockerfile详解](https://blog.csdn.net/AtlanSI/article/details/87892016)
---
+++ 01/dockerfile-example/daemon-01/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/pprof"
+	"os"
+	"strings"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", mainIndex)
+	mux.HandleFunc("/healthz", healthz)
+
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func mainIndex(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Header {
+		for _, vv := range v {
+			w.Header().Add(k, vv)
+		}
+	}
+	w.Header().Add("systemEnv", os.Getenv("GOPATH"))
+	cliIP := clientIP(r)
+	log.Printf("Success! Response code: %d client ip: %s", http.StatusOK, cliIP)
+}
+
+func clientIP(r *http.Request) string {
+	ip := strings.TrimSpace(strings.Split(r.Header.Get("x-Forwarded-For"), ",")[0])
+	if len(ip) != 0 {
+		return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if len(ip) != 0 {
+		return ip
+	}
+	ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr))
+	if err != nil {
+		return ""
+	}
+	return ip
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, fmt.Sprintf("%d", http.StatusOK))
+}
+
+
+
+
